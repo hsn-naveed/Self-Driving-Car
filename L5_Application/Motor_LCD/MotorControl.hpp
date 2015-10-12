a680665525c5b1f9a920a24cef0da1f53e3de033
hpradogu <hpradogu.csu@gmail.com>
2015-10-12 01:08:44 -0700

    In-Progress: Setting up MotorControl class with member variables/functions. Still not completed yet.

---
+++ L5_Application/Motor_LCD/MotorControl.hpp
@@ -0,0 +1,37 @@
+/*
+ * MotorControl.hpp
+ *
+ * Created by Jashan and Hector
+ *
+ *
+ */
+
+#ifndef MOTORCONTROL_HPP_
+#define MOTORCONTROL_HPP_
+
+#include "lpc_pwm.hpp"
+#include "scheduler_task.hpp"
+#include "FreeRTOS.h"
+#include "io.hpp"
+
+
+class MotorControl : public scheduler_task{
+    public:
+        PWM motorPwm;
+        PWM servoPwm;
+        float servoValue;
+        float motorValue;
+
+        MotorControl(PWM &motorPwmToSet, PWM &servoPwmToSet, float motorValueToSet, float servoValueToSet, uint8_t priorityToUse);
+        bool run(void *p);
+
+        // In case motor needs to be pulsed to get going prior to starting
+        void initCarMotor();
+
+        #if 1   // Motor Control Functions
+
+        // Parameters are subject to change
+        void forward(int speed, int duration);
+        void back(int speed, int duration);
+        #endif
+};
