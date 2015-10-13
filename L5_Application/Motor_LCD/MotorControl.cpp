7aa240c37916f3b6f74cc2b857134f99595dd407
hpradogu <hpradogu.csu@gmail.com>
2015-10-13 15:03:15 -0700

    In-Progress: Added more member functions/variables to MotorControl class. Still needs fleshing out of functions, and parameters corrected.
    MotorControl.cpp:
    1. Added in servo control functions

---
+++ L5_Application/Motor_LCD/MotorControl.cpp
@@ -0,0 +1,13 @@
+/*
+ * MotorControl.cpp
+ *
+ * Created by Jashan and Hector
+ *
+ *
+ */
+
+#include "MotorControl.hpp"
+
+MotorControl::MotorControl(PWM &motorPwmToSet, PWM &servoPwmToSet, uint8_t priorityToUse){
+
+}
--- L5_Application/Motor_LCD/MotorControl.hpp
+++ L5_Application/Motor_LCD/MotorControl.hpp
@@ -14,24 +14,47 @@
 #include "FreeRTOS.h"
 #include "io.hpp"
 
+struct{
+        float SLOW_SPEED = 76.7;
+        float FAST_SPEED = 79.3;
+        float BACK_SPEED = 70;
+        float MAX_SPEED = 96;
+        float STOP = 71.3;
+} speedSetting_t;
+
+struct{
+        float FULL_LEFT = 96;
+        float FULL_RIGHT = 48;
+        float STRAIGHT = 72;
+} steeringDirection_t;
 
 class MotorControl : public scheduler_task{
+    private:
+        float currentMotorValue;
+        float currentServoValue;
+
+        void setSteeringDirectionAndSpeed();
+
+        bool motorHasBeenInitialized = false;
     public:
         PWM motorPwm;
         PWM servoPwm;
-        float servoValue;
-        float motorValue;
 
-        MotorControl(PWM &motorPwmToSet, PWM &servoPwmToSet, float motorValueToSet, float servoValueToSet, uint8_t priorityToUse);
+        MotorControl(PWM &motorPwmToSet, PWM &servoPwmToSet, uint8_t priorityToUse);
         bool run(void *p);
 
         // In case motor needs to be pulsed to get going prior to starting
         void initCarMotor();
 
         #if 1   // Motor Control Functions
-
         // Parameters are subject to change
         void forward(int speed, int duration);
         void back(int speed, int duration);
+        #endif
+
+        #if 1   // Servo Steering functions
+        // Parameters are subject to change
+        void steerLeft(float amountToSteer, float distance);
+        void steerRight(float amountToSteer, float distance);
         #endif
 };
