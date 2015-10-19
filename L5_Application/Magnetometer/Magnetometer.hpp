7bd735cbf113f1e4c267cc9cc00dd963f856d016
Jonathon Hongpananon <jhongpananon@teslamotors.com>
2015-10-18 17:24:58 -0700

    Code Cleanup

--- L5_Application/Magnetometer/Magnetometer.hpp
+++ L5_Application/Magnetometer/Magnetometer.hpp
@@ -1,14 +1,10 @@
-/*
- * Magnetometer.hpp
- *
- *  Created on: Apr 27, 2015
- *      Author: calvin.lai
- */
-
 #ifndef L5_APPLICATION_MAGNETOMETER_MAGNETOMETER_HPP_
 #define L5_APPLICATION_MAGNETOMETER_MAGNETOMETER_HPP_
 
-class Magnetometer : public scheduler_task{
+
+
+class Magnetometer : public scheduler_task
+{
     public:
         Magnetometer(uint8_t priority);
         bool run(void *p);
