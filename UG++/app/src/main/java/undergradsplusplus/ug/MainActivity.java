deb2eebfc284c59a2077571ba630947b4dc08888
phiizo <philtrann@gmail.com>
2015-10-25 13:01:46 -0700

    Fixed the Fragments. Will no longer continuously add Fragments when button is pressed. Buttons will show/hide Fragments instead.

--- UG++/app/src/main/java/undergradsplusplus/ug/MainActivity.java
+++ UG++/app/src/main/java/undergradsplusplus/ug/MainActivity.java
@@ -27,59 +27,117 @@ public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "ug.undergradsplusplus.MESSAGE";
 
     FragmentManager manager;
+    FragmentTransaction transaction;
+
+    Setup_Fragment setupFrag = new Setup_Fragment();
+    Map_Fragment mapFrag = new Map_Fragment();
+    Status_Fragment statusFrag = new Status_Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        /*
+        *   This section of onCreate will initialize all the fragments.
+        *   After adding, it will hide the fragments. The user will put
+        *   the fragments to the foreground by pressing the buttons on the bottom.
+         */
         manager = getFragmentManager();
+
+        transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_layout, setupFrag, "SETUP");
+        transaction.add(R.id.fragment_layout, mapFrag, "MAP");
+        transaction.add(R.id.fragment_layout, statusFrag, "STATUS");
+        transaction.hide(setupFrag);
+        transaction.hide(mapFrag);
+        transaction.hide(statusFrag);
+        transaction.commit();
+
+
+
         Log.d("onMAIN", "IN MAIN NOW");
     }
 
+
+    /*
+    *   Setup Button. Created in XML.
+     */
     public void setupButton(View v)
     {
-        Setup_Fragment setupFrag = new Setup_Fragment();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.fragment_layout, setupFrag, "SETUP");
-        transaction.commit();
+        if(setupFrag.isHidden())
+        {
+            manager.beginTransaction().show(setupFrag).commit();
+
+            if(mapFrag.isVisible())
+                manager.beginTransaction().hide(mapFrag).commit();
+            else if(statusFrag.isVisible())
+                manager.beginTransaction().hide(statusFrag).commit();
+
+        }
+        else
+            manager.beginTransaction().hide(setupFrag).commit();
+
     }
 
+    /*
+    *   Map Button. Created in XML.
+     */
     public void mapButton(View v)
     {
-        Map_Fragment mapFrag = new Map_Fragment();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.fragment_layout, mapFrag, "MAP");
-        transaction.commit();
+        if(mapFrag.isHidden())
+        {
+            manager.beginTransaction().show(mapFrag).commit();
+
+            if(setupFrag.isVisible())
+                manager.beginTransaction().hide(setupFrag).commit();
+            else if(statusFrag.isVisible())
+                manager.beginTransaction().hide(statusFrag).commit();
+
+        }
+        else
+            manager.beginTransaction().hide(mapFrag).commit();
     }
 
+    /*
+    *   Status Button. Created in XML.
+     */
     public void statusButton(View v)
     {
-        Status_Fragment statusFrag = new Status_Fragment();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.fragment_layout, statusFrag, "STATUS");
-        transaction.commit();
-    }
+        if(statusFrag.isHidden())
+        {
+            manager.beginTransaction().show(statusFrag).commit();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            if(setupFrag.isVisible())
+                manager.beginTransaction().hide(setupFrag).commit();
+            else if(mapFrag.isVisible())
+                manager.beginTransaction().hide(mapFrag).commit();
         }
-
-        return super.onOptionsItemSelected(item);
+        else
+            manager.beginTransaction().hide(statusFrag).commit();
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
