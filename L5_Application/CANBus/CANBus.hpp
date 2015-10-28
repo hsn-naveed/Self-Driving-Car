479d000f5f39fd99975bc89210098350e9efba6a
hpradogu <hpradogu.csu@gmail.com>
2015-10-27 23:39:31 -0700

    Hopefully deleted submodules. In the middle of CAN addition

---
+++ L5_Application/CANBus/CANBus.hpp
@@ -0,0 +1,21 @@
+/*
+ * CANBus.hpp
+ */
+
+#include <stdio.h>
+#include "tasks.hpp"
+#include "can.h"
+#include "shared_handles.h"
+
+class CANBus :public scheduler_task{
+    private:
+        can_t canBusObj;
+        uint32_t canBusBaudRate_Kbps = 100;
+        uint16_t canBusRxQueueSize = 50;
+        uint16_t canBusTxQueueSize = 50;
+    public:
+        CANBus(uint8_t priorityToUse);
+
+        bool init(void);
+        bool run(void *p);
+};
--- L5_Application/shared_handles.h
+++ L5_Application/shared_handles.h
@@ -34,6 +34,13 @@
 enum {
     shared_SensorQueue,    ///< Shared handle used by examples (producer and consumer tasks)
     shared_learnSemaphore, ///< Terminal command gives this semaphore to remoteTask (IR sensor task)
+
+
+    /*
+     * Handles for CANBus queues
+     */
+    shared_CAN_Queue_Rx,
+    shared_CAN_Queue_Tx,
 };
 
 
