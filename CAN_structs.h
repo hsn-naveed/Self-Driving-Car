f4e180a060095687e5e9a3608e2fb4cc08ce38cc
Jonathon Hongpananon <jhongpananon@teslamotors.com>
2015-10-27 22:41:34 -0700

    First GPS struct message commit

--- CAN_structs.h
+++ CAN_structs.h
@@ -3,6 +3,15 @@
 
 
 
+// MSG ID: 0x362
+typedef struct {
+    uint64_t heading : 8;
+} gps_heading_msg_t;
 
+// MSG ID: 0x###
+typedef struct {
+    uint64_t x_coordinate : 8;
+    uint64_t y_coordinate : 8;
+} gps_coordinate_msg_t;
 
 #endif /* L5_APPLICATION_CAN_STRUCTS_H_ */
