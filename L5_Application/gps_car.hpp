8b22e93dd4852bd84999d3f4e0ecd91d77ab9b31
Jonathon Hongpananon <jhongpananon@teslamotors.com>
2015-10-27 18:13:26 -0700

    Added hearbeat function and changed API name to gps_car

--- L5_Application/gps_car.hpp
+++ L5_Application/gps_car.hpp
@@ -1,11 +1,11 @@
-#ifndef L5_APPLICATION_CAR_CAN_HPP_
-#define L5_APPLICATION_CAR_CAN_HPP_
+#ifndef L5_APPLICATION_GPS_CAR_HPP_
+#define L5_APPLICATION_GPS_CAR_HPP_
 #include "can.h"
 
 
 
 /// Initializes the can bus with FullCAN
-void car_can_init_can_bus(void);
+void gps_car_init_can_bus(void);
 
 /// Callback function if the bus is heavy
 void * bus_off_cb(void);
@@ -16,19 +16,23 @@ void * bus_off_cb(void);
  * @param [in] msg_id   Message ID of the CAN msg to retrieve
  * @returns true if the received msg id is the same as the input parameter
  */
-bool car_can_rx(can_fullcan_msg_t *fc1, uint16_t msg_id);
+bool gps_car_rx(can_fullcan_msg_t *fc1, uint16_t msg_id);
 
 /**
  * Sends a CAN msg so the user only has to fill in the data bytes
  * @param [in] msg      Pointer to the can_msg_t message
  * @param [in] msg_id   Message id of the CAN msg being sent
  */
-bool car_can_tx(can_msg_t *msg, uint32_t msg_id);
+bool gps_car_tx(can_msg_t *msg, uint32_t msg_id);
 
 /**
  * Sends the heading direction
- * @returns true if msg sent successfully
  */
-void car_can_send_heading(void);
+void gps_car_send_heading(void);
 
-#endif /* L5_APPLICATION_CAR_CAN_HPP_ */
+/**
+ * Sends the periodic heartbeat to the Master controller
+ */
+void gps_car_send_heartbeat(void);
+
+#endif /* L5_APPLICATION_GPS_CAR_HPP_ */
