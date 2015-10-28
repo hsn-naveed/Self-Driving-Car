e5d62a7a108c1009b346ce1efa48e4d868eb32a3
hpradogu <hpradogu.csu@gmail.com>
2015-10-27 23:57:47 -0700

    Commented out init function on CANBus.cpp

--- L5_Application/CANBus/CANBus.cpp
+++ L5_Application/CANBus/CANBus.cpp
@@ -8,16 +8,16 @@ CANBus::CANBus(uint8_t priorityToUse) : scheduler_task("canBusTask", 2048, prior
     canBusObj = can1;
 }
 
-bool CANBus::init(void){
-    if (CAN_init(canBusObj, canBusBaudRate_Kbps, canBusRxQueueSize, canBusTxQueueSize,
-            NULL, NULL)){
-        if (CAN_fullcan_add_entry(canBusObj, CAN_gen_sid(canBusObj, 0x8), CAN_gen_sid(canBusObj, 0x9))){
-            const can_std_id_t sList[] = {
-                   CAN_gen_sid(canBusObj, 0x8), CAN_gen_sid(canBusObj, 0x9),
-                    CAN_get_sid(canBusObj, 0xA), CAN_get_sid(canBusObj, 0xB)};
-            };
-            const can_std_grp_id_t sgList[] = { CAN_gen_sid(canBusObj, )
-        }
-    }
-}
+//bool CANBus::init(void){
+//    if (CAN_init(canBusObj, canBusBaudRate_Kbps, canBusRxQueueSize, canBusTxQueueSize,
+//            NULL, NULL)){
+//        if (CAN_fullcan_add_entry(canBusObj, CAN_gen_sid(canBusObj, 0x8), CAN_gen_sid(canBusObj, 0x9))){
+//            const can_std_id_t sList[] = {
+//                   CAN_gen_sid(canBusObj, 0x8), CAN_gen_sid(canBusObj, 0x9),
+//                    CAN_get_sid(canBusObj, 0xA), CAN_get_sid(canBusObj, 0xB)};
+//            };
+//            const can_std_grp_id_t sgList[] = { CAN_gen_sid(canBusObj, )
+//        }
+//    }
+//}
 
