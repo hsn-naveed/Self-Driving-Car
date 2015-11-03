893a64759ddb36865092512a3e36e7a5a9d4e826
Jonathon Hongpananon <jhongpananon@teslamotors.com>
2015-11-02 19:38:26 -0800

    Submodule update

---
+++ 243_can/CAN_structs.h
@@ -0,0 +1,18 @@
+#ifndef L5_APPLICATION_CAN_STRUCTS_H_
+#define L5_APPLICATION_CAN_STRUCTS_H_
+
+
+
+
+const uint32_t HEADING_MSG_RECV_ID = 0x362;
+typedef struct {
+    uint64_t heading : 8;
+} gps_heading_msg_t;
+
+// MSG ID: 0x###
+typedef struct {
+    uint64_t x_coordinate : 8;
+    uint64_t y_coordinate : 8;
+} gps_coordinate_msg_t;
+
+#endif /* L5_APPLICATION_CAN_STRUCTS_H_ */
