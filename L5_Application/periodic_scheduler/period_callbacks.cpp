991744e7dc7fa4b7c398c63566b7c651b8fc8f3c
nivram07 <marvinb_flores@yahoo.com>
2015-11-10 20:38:27 -0800

    demo code

--- L5_Application/periodic_scheduler/period_callbacks.cpp
+++ L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -47,6 +47,8 @@
 #include "243_can/CAN_structs.h"
 #include "243_can/iCAN.hpp"
 
+
+
 // set to 2 if you want to enable motor_control with switches
 // set to 0 if you want to use CAN
 #define DEBUG_NO_CAN 0
@@ -56,7 +58,7 @@
 const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
 
 const int g_reset = 0;
-const int g_max_count_timer = 10; // we're running in 100Hz and we expect messages within 10Hz.
+const int g_max_count_timer = 300; // we're running in 100Hz and we expect messages within 3Hz.
 
 //currently not used
 uint8_t current_mode = 0;
@@ -82,12 +84,14 @@ static const int CHECK_SENSOR_VALUES = 0;
 static const int CONTROL_BASED_ON_SENSOR = 1;
 static const int CONTROL_BASED_ON_HEADING = 2;
 static const int SEND_CONTROL_TO_MOTOR = 3;
+static const int STATE_MACHINE_END = 4;
 
 //Sensor readings based on minimum values
 bool LEFT_BLOCKED = false;
 bool RIGHT_BLOCKED = false;
 bool MIDDLE_BLOCKED = false;
 bool BACK_BLOCKED = false;
+bool CRASH_DISTANCE_BLOCKED = false;
 
 //if all three are clear
 bool FRONT_CLEAR = false;
@@ -153,6 +157,12 @@ void printMotorCommand(int print_command)    {
 
 void parseSensorReading(sen_msg_t* data)  {
 
+    if (data->L < CRASH_AVOID_DISTANCE ||
+            data->M < CRASH_AVOID_DISTANCE ||
+            data->R < CRASH_AVOID_DISTANCE    ) {
+        CRASH_DISTANCE_BLOCKED = true;
+    } else  CRASH_DISTANCE_BLOCKED = false;
+
     if (data->L < MINIMUM_SENSOR_VALUE ) LEFT_BLOCKED = true;
         else LEFT_BLOCKED = false;
 
@@ -161,10 +171,6 @@ void parseSensorReading(sen_msg_t* data)  {
 
     if (data->R < MINIMUM_SENSOR_VALUE ) RIGHT_BLOCKED = true;
        else RIGHT_BLOCKED = false;
-
-    if (data->L < MINIMUM_SENSOR_VALUE ) LEFT_BLOCKED = true;
-       else LEFT_BLOCKED = false;
-
 
     if (data->B < MINIMUM_SENSOR_BLOCKED_VALUE ) BACK_BLOCKED = true;
        else BACK_BLOCKED = false;
@@ -178,45 +184,45 @@ void generateMotorCommands(int command)    {
 
     switch(command) {
         case COMMAND_MOTOR_STOP:
-            CAN_ST.motor_data->FRS = (uint8_t) COMMAND_STOP;
+           // CAN_ST.motor_data->FRS = (uint8_t) COMMAND_STOP;
             CAN_ST.motor_data->LR = (uint8_t) COMMAND_STRAIGHT;
-            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_SLOW;
+            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_STOP;
 
             break;
 
         case COMMAND_MOTOR_FORWARD_STRAIGHT:
-            CAN_ST.motor_data->FRS = (uint8_t) COMMAND_FORWARD;
+            //CAN_ST.motor_data->FRS = (uint8_t) COMMAND_FORWARD;
             CAN_ST.motor_data->LR = (uint8_t) COMMAND_STRAIGHT;
             CAN_ST.motor_data->SPD = (uint8_t) COMMAND_MEDIUM;
             break;
 
         case COMMAND_MOTOR_FORWARD_LEFT:
-            CAN_ST.motor_data->FRS = (uint8_t) COMMAND_FORWARD;
+           // CAN_ST.motor_data->FRS = (uint8_t) COMMAND_FORWARD;
             CAN_ST.motor_data->LR = (uint8_t) COMMAND_LEFT;
             CAN_ST.motor_data->SPD = (uint8_t) COMMAND_MEDIUM;
             break;
 
         case COMMAND_MOTOR_FORWARD_RIGHT:
-            CAN_ST.motor_data->FRS = (uint8_t) COMMAND_FORWARD;
+          //  CAN_ST.motor_data->FRS = (uint8_t) COMMAND_FORWARD;
             CAN_ST.motor_data->LR = (uint8_t) COMMAND_RIGHT;
             CAN_ST.motor_data->SPD = (uint8_t) COMMAND_MEDIUM;
             break;
 
         case COMMAND_MOTOR_REVERSE_LEFT:
-            CAN_ST.motor_data->FRS = (uint8_t) COMMAND_REVERSE;
+          //  CAN_ST.motor_data->FRS = (uint8_t) COMMAND_REVERSE;
             CAN_ST.motor_data->LR = (uint8_t) COMMAND_LEFT;
-            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_SLOW;
+            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_STOP; //change this for reverse!
             break;
 
         case COMMAND_MOTOR_REVERSE_RIGHT:
-            CAN_ST.motor_data->FRS = (uint8_t) COMMAND_REVERSE;
+           // CAN_ST.motor_data->FRS = (uint8_t) COMMAND_REVERSE;
             CAN_ST.motor_data->LR = (uint8_t) COMMAND_RIGHT;
-            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_SLOW;
+            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_STOP; //change this for reverse!
             break;
         default:
-            CAN_ST.motor_data->FRS = (uint8_t) COMMAND_STOP;
+           // CAN_ST.motor_data->FRS = (uint8_t) COMMAND_STOP;
             CAN_ST.motor_data->LR = (uint8_t) COMMAND_STRAIGHT;
-            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_SLOW;
+            CAN_ST.motor_data->SPD = (uint8_t) COMMAND_STOP;
             break;
     }
     printMotorCommand(command);
@@ -228,7 +234,7 @@ void generateMotorCommands(int command)    {
 void period_1Hz(void)
 {
     LE.toggle(1);
-    printf("HEART BEAT: %d\n", g_heart_counter++);
+    printf("\n\n\nHEART BEAT: %d\n\n\n", g_heart_counter++);
     if (10000 < g_heart_counter) g_heart_counter = 0;
 
 }
@@ -238,16 +244,26 @@ int g_sensor_receive_counter = 0;
 
 void period_10Hz(void)
 {
+    sen_msg_t *l_sensor_values;
+
     portDISABLE_INTERRUPTS();
     //copy our global values to local values
-    sen_msg_t *l_sensor_values = CAN_ST.sensor_data;
+    //sen_msg_t *l_sensor_values = CAN_ST.sensor_data;
+
+    l_sensor_values = CAN_ST.sensor_data;
+    //printf("COPY TOOK PLACE %d %d %d %d\n", (int) CAN_ST.sensor_data->L, (int) CAN_ST.sensor_data->M, (int) CAN_ST.sensor_data->R, (int) CAN_ST.sensor_data->B );
     portENABLE_INTERRUPTS();
+
 
     //for testing
     uint8_t temp[4];
 
 
     CAN_ST.motor_data = (mast_mot_msg_t*) & msg_tx.data.bytes[0];
+
+    g_current_state = 0;
+
+    while(g_current_state != STATE_MACHINE_END) {
 
 
     switch(g_current_state){
@@ -260,8 +276,9 @@ void period_10Hz(void)
                 temp[1] = (uint8_t) l_sensor_values->M;
                 temp[2] = (uint8_t) l_sensor_values->R;
                 temp[3] = (uint8_t) l_sensor_values->B;
-                printf("%d %d %d %d\n", temp[0], temp[1], temp[2], temp[3]);
+                printf("            L: %d   M: %d   R: %d   B: %d\n", temp[0], temp[1], temp[2], temp[3]);
 
+                //we parse the sensor values
                 parseSensorReading(l_sensor_values);
                 if(FRONT_CLEAR) g_current_state = CONTROL_BASED_ON_HEADING;
                 else g_current_state = CONTROL_BASED_ON_SENSOR;
@@ -270,9 +287,17 @@ void period_10Hz(void)
 
         case CONTROL_BASED_ON_SENSOR:
             g_prev_state = g_current_state;
-
+            //if critical distance is detected
+            if (CRASH_DISTANCE_BLOCKED) {
+                generateMotorCommands(COMMAND_MOTOR_STOP);
+            }
+            //if middle is blocked
+            else if (MIDDLE_BLOCKED) {
+                generateMotorCommands(COMMAND_MOTOR_STOP);
+                LD.clear();
+            }
             // only left is blocked
-           if (LEFT_BLOCKED && !RIGHT_BLOCKED && !MIDDLE_BLOCKED)  {
+            else if (LEFT_BLOCKED && !RIGHT_BLOCKED && !MIDDLE_BLOCKED)  {
                   generateMotorCommands(COMMAND_MOTOR_FORWARD_RIGHT); //turn right
                   LD.clear();
            }
@@ -283,22 +308,26 @@ void period_10Hz(void)
            }
            //only middle is blocked
            else if(MIDDLE_BLOCKED && !LEFT_BLOCKED && !RIGHT_BLOCKED)    {
-               generateMotorCommands(COMMAND_MOTOR_FORWARD_RIGHT); //turn right
+             //  generateMotorCommands(COMMAND_MOTOR_FORWARD_RIGHT); //turn right
+               generateMotorCommands(COMMAND_MOTOR_STOP);
                LD.clear();
            }
            //if left and middle is blocked
            else if (LEFT_BLOCKED && MIDDLE_BLOCKED && !RIGHT_BLOCKED)   {
-               generateMotorCommands(COMMAND_MOTOR_FORWARD_RIGHT);  //turn right
+               //generateMotorCommands(COMMAND_MOTOR_FORWARD_RIGHT);  //turn right
+               generateMotorCommands(COMMAND_MOTOR_STOP);
                LD.clear();
            }
            //if right and middle is blocked
            else if (RIGHT_BLOCKED && MIDDLE_BLOCKED && !LEFT_BLOCKED)   {
-               generateMotorCommands(COMMAND_MOTOR_FORWARD_LEFT); //turn left
+               //generateMotorCommands(COMMAND_MOTOR_FORWARD_LEFT); //turn left
+               generateMotorCommands(COMMAND_MOTOR_STOP);
                LD.clear();
            }
            //if left and right are blocked
            else if(LEFT_BLOCKED && RIGHT_BLOCKED && !MIDDLE_BLOCKED){
-               generateMotorCommands(COMMAND_MOTOR_FORWARD_STRAIGHT); //keep going straight
+               //generateMotorCommands(COMMAND_MOTOR_FORWARD_STRAIGHT); //keep going straight
+               generateMotorCommands(COMMAND_MOTOR_STOP);
                LD.clear();
            }
            //if all front sensors are blocked
@@ -315,6 +344,10 @@ void period_10Hz(void)
                    generateMotorCommands(COMMAND_MOTOR_REVERSE_LEFT);
                    LD.clear();
                }
+           }
+           else {
+               generateMotorCommands(COMMAND_MOTOR_STOP);
+               LD.clear();
            }
 
             g_current_state = SEND_CONTROL_TO_MOTOR;
@@ -340,30 +373,32 @@ void period_10Hz(void)
             g_prev_state = g_current_state;
 
             //for debugging
-            printf("SENDING FRS:%2x LR:%2x SPD:%2x\n", msg_tx.data.bytes[0],  msg_tx.data.bytes[1],  msg_tx.data.bytes[2] );
+           // printf("SENDING FRS:%2x LR:%2x SPD:%2x\n", msg_tx.data.bytes[0],  msg_tx.data.bytes[1],  msg_tx.data.bytes[2] );
 
             //prepare our message id
             msg_tx.msg_id = (uint32_t) MASTER_COMMANDS_MOTOR;
             //send our message
             if(iCAN_tx(&msg_tx, (uint16_t) MASTER_COMMANDS_MOTOR))   {
-                printf("Message sent to motor!\n");
+               printf("Message sent to motor!\n");
             }
 
             //reset our state
-            g_current_state = CHECK_SENSOR_VALUES;
+           // g_current_state = CHECK_SENSOR_VALUES;
+            g_current_state = STATE_MACHINE_END;
             LD.clear();
             break;
 
         default:
-            g_current_state = CHECK_SENSOR_VALUES;
+          //  g_current_state = CHECK_SENSOR_VALUES;
+            g_current_state = STATE_MACHINE_END;
             LD.clear();
             break;
 
 
     } //end switch(g_current_state)
 
-
-    delete l_sensor_values;
+    }//end while
+   // delete l_sensor_values;
 
 } // end 10Hz task
 
@@ -371,23 +406,24 @@ void period_100Hz(void)
 {
 
     can_fullcan_msg_t *temp_rx = new can_fullcan_msg_t{0};
-
+   // can_fullcan_msg_t *temp_rx;
         //RECEIVE AND SAVE FULL_CAN MESSAGES
         if(iCAN_rx(temp_rx, (uint16_t) SENSOR_MASTER_REG))    {
 
             portDISABLE_INTERRUPTS();
 
-            CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
+          //  CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
             //if this ^ does not work try to uncomment these:
-                    /*CAN_ST.sensor_data->L temp_rx->data.bytes[0];
-                    CAN_ST.sensor_data->M temp_rx->data.bytes[1];
-                    CAN_ST.sensor_data->R temp_rx->data.bytes[2];
-                    CAN_ST.sensor_data->B temp_rx->data.bytes[3];    */
+                  CAN_ST.sensor_data->L = (uint8_t) temp_rx->data.bytes[0];
+                    CAN_ST.sensor_data->M =(uint8_t) temp_rx->data.bytes[1];
+                    CAN_ST.sensor_data->R = (uint8_t) temp_rx->data.bytes[2];
+                    CAN_ST.sensor_data->B =  (uint8_t) temp_rx->data.bytes[3];
 
             portENABLE_INTERRUPTS();
 
             printf("SENSOR VAL READ!\n");
-
+           // printf("NEW VALUES: %d\n", (int) CAN_ST.sensor_data->L);
+           // printf("NEW VALUES %d %d %d %d\n", (int) CAN_ST.sensor_data->L, (int) CAN_ST.sensor_data->M, (int) CAN_ST.sensor_data->R, (int) CAN_ST.sensor_data->B );
             //g_reset counter because we received a message
             g_sensor_receive_counter = g_reset;
         }
@@ -496,6 +532,7 @@ void period_100Hz(void)
             CAN_ST.sensor_data->M = (uint8_t) 0x00;
             CAN_ST.sensor_data->R = (uint8_t) 0x00;
             CAN_ST.sensor_data->B = (uint8_t) 0x00;
+            portENABLE_INTERRUPTS();
             break;
 
         default:
@@ -516,7 +553,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0xff;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
+            portENABLE_INTERRUPTS();
             break;
+
         case 1:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0x00;
@@ -524,9 +563,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0xff;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
+
         case 2:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0xff;
@@ -534,9 +573,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0xff;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
+
         case 3:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0x00;
@@ -544,9 +583,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0xff;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
+
         case 4:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0xff;
@@ -554,9 +593,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0x00;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
+
         case 5:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0x00;
@@ -564,9 +603,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0x00;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
+
         case 6:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0xff;
@@ -574,9 +613,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0x00;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
+
         case 7:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0x00;
@@ -584,9 +623,9 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0x00;
             temp_rx->data.bytes[3] = (uint8_t) 0xff;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
+
         case 8:
             portDISABLE_INTERRUPTS();
             temp_rx->data.bytes[0] = (uint8_t) 0xff;
@@ -594,7 +633,6 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0xff;
             temp_rx->data.bytes[3] = (uint8_t) 0x00;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
             portENABLE_INTERRUPTS();
             break;
 
@@ -605,7 +643,7 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0x00;
             temp_rx->data.bytes[3] = (uint8_t) 0x00;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
-
+            portENABLE_INTERRUPTS();
             break;
 
         default:
@@ -615,6 +653,7 @@ void period_100Hz(void)
             temp_rx->data.bytes[2] = (uint8_t) 0x00;
             temp_rx->data.bytes[3] = (uint8_t) 0x00;
             CAN_ST.sensor_data = (sen_msg_t*) & temp_rx->data.bytes[0];
+            portENABLE_INTERRUPTS();
             break;
     }
 #endif
