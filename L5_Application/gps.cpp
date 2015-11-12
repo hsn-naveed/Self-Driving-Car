4b1ed3c87e79b552a53c47d75bf20aeb45945280
Hassan <hassan.jehangeri@gmail.com>
2015-11-11 22:29:27 -0800

    Calculating the heading function

--- L5_Application/gps.cpp
+++ L5_Application/gps.cpp
@@ -11,8 +11,24 @@
 #include "math.h"
 
 #define Pi 3.14159265
-uint64_t curr_heading(){
-    return(atan((CAN_ST.gps_coords_dest->y_coordinate-CAN_ST.gps_coords_curr->y_coordinate)/(CAN_ST.gps_coords_dest->x_coordinate-CAN_ST.gps_coords_curr->x_coordinate)) * 180/Pi);
-}
+uint32_t curr_heading(){
+    float degrees = (atan((CAN_ST.gps_coords_dest->y_coordinate-CAN_ST.gps_coords_curr->y_coordinate)/(CAN_ST.gps_coords_dest->x_coordinate-CAN_ST.gps_coords_curr->x_coordinate)) * 180/Pi);
 
+    //First quadrant (dest.y-curr.y)>0 and (dest.x-curr.x)>0
+    if (((CAN_ST.gps_coords_dest->y_coordinate-CAN_ST.gps_coords_curr->y_coordinate)>0) && ((CAN_ST.gps_coords_dest->x_coordinate-CAN_ST.gps_coords_curr->x_coordinate)>0)){
+        return degrees;
+    }
+    //Second quadrant (dest.y-curr.y)>0 and (dest.x-curr.x)<0
+    else if(((CAN_ST.gps_coords_dest->y_coordinate-CAN_ST.gps_coords_curr->y_coordinate))>0 && ((CAN_ST.gps_coords_dest->x_coordinate-CAN_ST.gps_coords_curr->x_coordinate)<0)){
+        return (180 - degrees );
+    }
+    //Third quadrant (dest.y-curr.y)<0 and (dest.x-curr.x)<0
+    else if(((CAN_ST.gps_coords_dest->y_coordinate-CAN_ST.gps_coords_curr->y_coordinate)<0) && ((CAN_ST.gps_coords_dest->x_coordinate-CAN_ST.gps_coords_curr->x_coordinate)<0)){
+            return (180 + degrees );
+        }
+   //Fourth Quadrant
+    else {
+                return (360 - degrees );
+    }
+}
 
