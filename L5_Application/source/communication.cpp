211352700f2f2233183fe9ca6c3242324af222c3
sming <wange04@gmail.com>
2015-11-28 12:38:04 -0800

    Bluetooth Recieve

--- L5_Application/source/communication.cpp
+++ L5_Application/source/communication.cpp
@@ -5,28 +5,54 @@
  *      Author: mac
  */
 
-#include "tasks.hpp"
 #include "communication.hpp"
-#include <stdio.h>
-#include "io.hpp"
 
 
-
-bool bluetoothTask::run(void *p)
+bool bluetoothRecieve::run(void *p)
 {
-
-
+    char input;
     if (bluetoothUart.getChar(&input, 100))
     {
-        putchar(input);
+        bluetooth_list.push_back(input);
     }
+    float x1;
 
-
-    if (SW.getSwitch(1))
+    if (bluetooth_list.size() == 4)
     {
-        bluetoothUart.putChar('s');
-        printf("Sending.....");
+        x1 = bluetooth_list[0] << 0 | bluetooth_list[1] << 8 | bluetooth_list[2] << 16 | bluetooth_list[3] << 24;
+        printf("%f \n", x1);
     }
+
+    /*char tempChar;
+    bluetoothUart.getChar(&tempChar, portMAX_DELAY);
+    input[inputCounter] = tempChar;
+    inputCounter++;
+
+    if (inputCounter >= 7){
+        for (int i = 0; i < inputCounter; i++){
+            printf("input[%i] = %c\n", inputCounter, (char)(input[i] & 0xFF));
+        }
+        inputCounter = 0;
+    }
+    else{
+        for (int i = 0; i < inputCounter; i++)
+        printf("input[%i] = %c\n", inputCounter, (char)(input[i] & 0xFF));
+    }*/
+
+
+//        if (strncmp(input, GO_Msg, sizeof(GO_Msg)) == 0)
+//            printf("\n%s\n", input);
+//        else if (strncmp(input, STOP_Msg, sizeof(STOP_Msg)) == 0)
+//            printf("\n%s\n", input);
+//        else if (strncmp(input, STOP_Msg, sizeof(BTS)) == 0 || strncmp(input, STOP_Msg, sizeof(CON)) == 0)
+//            return true;
+//        else
+//        {
+//            bluetooth_list.push_back(input);
+//            printf("%c\n", bluetooth_list[0]);
+//        }
+
+    //printf("\n\nsize: %d\n\n", bluetooth_list.size());
 
     return true;
 }
