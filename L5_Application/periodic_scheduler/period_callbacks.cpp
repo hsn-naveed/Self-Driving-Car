b355b78252d118cfed9e96c513aee4e8e7214c2b
sming <wange04@gmail.com>
2015-12-13 23:30:26 -0800

    Removed for loop

--- L5_Application/periodic_scheduler/period_callbacks.cpp
+++ L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -35,13 +35,12 @@
 #include "file_logger.h"
 #include "243_can/iCAN.hpp"
 #include "243_can/CAN_structs.h"
-#include "can.h"
 
 static int count;
 
 /***************** ANDROID *****************/
-extern ANDROID_TX_STOP_GO_CMD_t android_stop_go_values;
-extern ANDROID_TX_INFO_CHECKPOINTS_t android_checkpoints_values;
+extern ANDROID_TX_STOP_GO_CMD_t *android_stop_go_values;
+extern ANDROID_TX_INFO_CHECKPOINTS_t *android_checkpoints_values;
 extern ANDROID_TX_INFO_COORDINATES_t *android_coordinates_values;
 
 /***************** CAN MESSAGE *****************/
@@ -59,16 +58,27 @@ void canBusError1(){
 /// This is the stack size used for each of the period tasks
 const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
 
+
+can_fullcan_msg_t *temp;
+
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
-    uint32_t sglist[] = {ANDROID_TX_STOP_GO_CMD_HDR.mid, ANDROID_TX_INFO_CHECKPOINTS_HDR.mid, ANDROID_TX_INFO_COORDINATES_HDR.mid};
+    uint32_t sglist[] = {ANDROID_TX_STOP_GO_CMD_HDR.mid,
+                        ANDROID_TX_INFO_CHECKPOINTS_HDR.mid,
+                        ANDROID_TX_INFO_COORDINATES_HDR.mid};
     size_t sizeOfArray = (sizeof(sglist) / sizeof(*sglist));
     iCAN_init_FULLCAN(sglist, sizeOfArray);
 
     msg_tx = new can_msg_t{0};
+    android_stop_go_values = new ANDROID_TX_STOP_GO_CMD_t {0};
+    android_checkpoints_values = new ANDROID_TX_INFO_CHECKPOINTS_t {0};
     android_coordinates_values = new ANDROID_TX_INFO_COORDINATES_t {0};
-    android_stop_go_values.ANDROID_STOP_CMD_signal = (uint8_t)-1;
+    android_stop_go_values->ANDROID_STOP_CMD_signal = (uint8_t)-1;
+
+
+
+    temp = new can_fullcan_msg_t {0};
 
     return true; // Must return true upon success
 }
@@ -87,25 +97,19 @@ void period_1Hz(void)
 
 void period_10Hz(void)
 {
-    if (android_stop_go_values.ANDROID_STOP_CMD_signal == 0)
-    {
+
+    /*if (android_stop_go_values->ANDROID_STOP_CMD_signal == 0)
+    {*/
       /*  printf("%d %d %f %f\n", android_stop_go_values->ANDROID_STOP_CMD_signal,
                                     android_checkpoints_values->ANDROID_INFO_CHECKPOINTS_count,
                                     android_coordinates_values[1].GPS_INFO_COORDINATES_lat,
                                     android_coordinates_values[1].GPS_INFO_COORDINATES_long);
 */
-        msg_tx->msg_id = (uint32_t)ANDROID_TX_STOP_GO_CMD_HDR.mid;
-        msg_hdr_t encoded_message = ANDROID_TX_STOP_GO_CMD_encode((uint64_t*)&(msg_tx->data.qword), &android_stop_go_values);
-
-        if (iCAN_tx(msg_tx, &encoded_message))
-        {
-           printf("STOP message sent to master\n");
-        }
-    }
-    else if (android_stop_go_values.ANDROID_STOP_CMD_signal == 1)
+   /* }
+    else if (android_stop_go_values->ANDROID_STOP_CMD_signal == 1)
     {
         msg_tx->msg_id = (uint32_t)ANDROID_TX_STOP_GO_CMD_HDR.mid;
-        msg_hdr_t encoded_message = ANDROID_TX_STOP_GO_CMD_encode((uint64_t*)&(msg_tx->data.qword), &android_stop_go_values);
+        msg_hdr_t encoded_message = ANDROID_TX_STOP_GO_CMD_encode((uint64_t*)&(msg_tx->data.qword), android_stop_go_values);
 
         if (iCAN_tx(msg_tx, &encoded_message))
         {
@@ -113,17 +117,25 @@ void period_10Hz(void)
         }
 
         msg_tx->msg_id = (uint32_t)ANDROID_TX_INFO_CHECKPOINTS_HDR.mid;
-        encoded_message = ANDROID_TX_INFO_CHECKPOINTS_encode((uint64_t*)&(msg_tx->data.qword), &android_checkpoints_values);
+        encoded_message = ANDROID_TX_INFO_CHECKPOINTS_encode((uint64_t*)&(msg_tx->data.qword), android_checkpoints_values);
         if (iCAN_tx(msg_tx, &encoded_message))
         {
             printf("checkpoint sent!\n");
         }
-    }
+    }*/
 }
 
 void period_100Hz(void)
 {
     LE.toggle(3);
+    //msg_tx->msg_id = (uint32_t)ANDROID_TX_STOP_GO_CMD_HDR.mid;
+    msg_tx->msg_id = (uint32_t)700;
+    msg_hdr_t encoded_message = ANDROID_TX_STOP_GO_CMD_encode(&(msg_tx->data.qword), android_stop_go_values);
+
+    if (iCAN_tx(msg_tx, &encoded_message))
+    {
+       printf("CAN message sent!\n");
+    }
 
   /*  if ( count == android_checkpoints_values->ANDROID_INFO_CHECKPOINTS_count)
     {
@@ -131,12 +143,12 @@ void period_100Hz(void)
     }
     else
     {*/
-        msg_tx->msg_id = (uint32_t)ANDROID_TX_INFO_COORDINATES_HDR.mid;
+       /* msg_tx->msg_id = (uint32_t)ANDROID_TX_INFO_COORDINATES_HDR.mid;
         msg_hdr_t message = ANDROID_TX_INFO_COORDINATES_encode((uint64_t*)&(msg_tx->data.qword), &android_coordinates_values[count++]);
         if (iCAN_tx(msg_tx, &message))
         {
             printf("android_coordinates_values sent");
-        }
+        }*/
 
 }
 
